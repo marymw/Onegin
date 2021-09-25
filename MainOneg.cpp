1057f8b998468f2342be53e1b86ae2ae2b9ab037
marymw <71140577+marymw@users.noreply.github.com>
2021-09-25 13:14:24 +0300

    Update MainOneg.cpp

--- MainOneg.cpp
+++ MainOneg.cpp
@@ -5,34 +5,34 @@ int main(int argc, char *argv[]) {
 	int StatusArgCheck = ArgCheck(argc);
 	assert(StatusArgCheck == 0);
 	
-	PrintGreetings();//печатаем приветствие
+	PrintGreetings();														//печатаем приветствие
 	
-	MyString *Index  = nullptr;//пока не знаем размер массива структур
-	char     *buffer = nullptr;//будем читать туда файл
+	MyString *Index  = nullptr;												//пока не знаем размер массива структур
+	char     *buffer = nullptr;												//будем читать туда файл
 
 	
 	char *INPUT_FILE = argv[1];
 	char *OUTPUT_FILE = argv[2];
 
-	ReadFromFile(&buffer, INPUT_FILE);//записали весь файл в строку, это без \0
+	ReadFromFile(&buffer, INPUT_FILE);										//записали весь файл в строку, это без \0
 	assert(buffer);
 	
-	int NumberOfStrings = DecomposeToIndex(&Index, &buffer);//разложит всё в индекс из буфера 
+	int NumberOfStrings = DecomposeToIndex(&Index, &buffer);				//разложит всё в индекс из буфера 
 
-	qsort(Index, NumberOfStrings, sizeof(MyString), CompareByFirstLetters);//сортровка первого варианта, тут индекс должен быть указателем на первый элемент
+	qsort(Index, NumberOfStrings, sizeof(MyString), CompareByFirstLetters);	//сортровка первого вариантам
 
 	FILE *OutputFilePtr = OpenOutputFile(OUTPUT_FILE);
 
-	PrintSeparator(OutputFilePtr);//печатает разделитель
-	PrintToFile(OutputFilePtr, Index, NumberOfStrings);//печатает файл(с помощью индекса
-	PrintSeparator(OutputFilePtr);//печатает разделитель
+	PrintSeparator(OutputFilePtr);											//печатает разделитель
+	PrintToFile(OutputFilePtr, Index, NumberOfStrings);						//печатает файл(с помощью индекса
+	PrintSeparator(OutputFilePtr);											//печатает разделитель
 	
-	Myqsort(Index, 0, NumberOfStrings - 1, CompareByLastLetters);//сортировка второго варианта
+	Myqsort(Index, 0, NumberOfStrings - 1, CompareByLastLetters);			//сортировка второго варианта
 
-	PrintToFile(OutputFilePtr, Index, NumberOfStrings);//печатает файл
-	PrintSeparator(OutputFilePtr);//печатает разделитель
-	PrintBufferToFile (OutputFilePtr, buffer);//печатает исходный файл, можно попробовать модифицировать printfile()
-	PrintSeparator(OutputFilePtr);//печатает разделитель
+	PrintToFile(OutputFilePtr, Index, NumberOfStrings);						//печатает файл
+	PrintSeparator(OutputFilePtr);											//печатает разделитель
+	PrintBufferToFile (OutputFilePtr, buffer);								//печатает исходный файл
+	PrintSeparator(OutputFilePtr);											//печатает разделитель
 
 	free(Index);
 	free(buffer);
